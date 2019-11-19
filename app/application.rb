3971356a34f135456690b1c84ac991f1a365fb1d
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-11-19 16:05:59 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+
+    def call(env)
+        resp = Rack::Response.new
+
+
+if Time == 12
+    # ==num_2 && num_2==num_3
+    resp.write "Good Morning"
+  else
+    resp.write "Good Afternoon"
+  end
+
+end
